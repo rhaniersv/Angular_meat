5be40f5fde63bee3e1727e40471333b1f53e561a
Rhanier <rhaniersv@gmail.com>
2019-07-30 13:50:11 -0300

    first commit

---
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from "@angular/core";
+import { Observable } from "rxjs/Observable";
+import { MenuItem } from "../menu-item/menu-item.model";
+import { RestaurantsService } from "app/restaurants/restaurants.service";
+import { ActivatedRoute } from "@angular/router";
+
+@Component({
+  selector: "mt-menu",
+  templateUrl: "./menu.component.html"
+})
+export class MenuComponent implements OnInit {
+  menu: Observable<MenuItem[]>;
+
+  constructor(
+    private restaurantService: RestaurantsService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.menu = this.restaurantService.menuOfRestaurant(
+      this.route.parent.snapshot.params["id"]
+    );
+  }
+}
